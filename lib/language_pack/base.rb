d633b0e39e1c3a7be01eb15ea975d90d0fc34621
Padraic Doyle <padraic@padraicdoyle.com>
2017-08-10 10:22:47 -0400

    new base

--- lib/language_pack/base.rb
+++ lib/language_pack/base.rb
@@ -7,8 +7,7 @@ Encoding.default_external = Encoding::UTF_8 if defined?(Encoding)
 
 # abstract class that all the Ruby based Language Packs inherit from
 class LanguagePack::Base
-  #VENDOR_URL = "https://s3.amazonaws.com/heroku-buildpack-ruby"
-  VENDOR_URL = "https://heroku-buildpack-ruby.s3.amazonaws.com"
+  VENDOR_URL = "https://s3-external-1.amazonaws.com/heroku-buildpack-ruby"
 
   attr_reader :build_path, :cache_path
 
