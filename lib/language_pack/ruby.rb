f1064c7403ef9370f5d6740a5d167b713bbfe971
Padraic Doyle <padraic@padraicdoyle.com>
2017-08-10 10:15:24 -0400

    fix backquote

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -171,7 +171,8 @@ private
 
     Dir.mktmpdir("ruby_versions-") do |tmpdir|
       Dir.chdir(tmpdir) do
-        run("curl -O #{VENDOR_URL}/ruby_versions.yml")
+        run("curl -O #{VENDOR_URL
+        }/ruby_versions.yml")
         https://s3.amazonaws.com/heroku-pdftk-vendor/ruby_versions.yml
         @ruby_versions = YAML::load_file("ruby_versions.yml")
       end
